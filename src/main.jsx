558f4a7e3581268ef1e3922ad77c3da83cfd8949
Ismael Gustavo Da Silva <81600908+Snoopyh@users.noreply.github.com>
2023-04-18 11:58:32 -0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import Search from "./pages/Search";
 
